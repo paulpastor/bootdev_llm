b432369f000e81532f6727b2adac14bd81059af4
Paul Pastor <paulpastor@me.com>
2025-12-29 11:13:04 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+
+if response.usage_metadata:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+else:
+    raise RuntimeError("No usage metadata found in the response.")
