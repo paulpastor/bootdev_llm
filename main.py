fd0421f2fdc74b2ead251b7de15a7a1d794a4534
Paul Pastor <paulpastor@me.com>
2025-12-29 13:53:51 -0500

    added verbose arg

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,16 +15,21 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Generate content using Gemini API.")
 parser.add_argument("user_prompt", type=str, help="The prompt to generate content for.")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output.")
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=args.user_prompt,
+    contents=messages,
 )
 
 if response.usage_metadata:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 else:
     raise RuntimeError("No usage metadata found in the response.")
