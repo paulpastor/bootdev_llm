644e8457e3ef5131e4bdc2c6d99575397f61a3c4
Paul Pastor <paulpastor@me.com>
2025-12-30 10:27:08 -0500

    added system prompt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -23,6 +25,7 @@ messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)]
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
 if response.usage_metadata:
