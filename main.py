f200b992b11ba5072827cc47018518642c37f7e0
Paul Pastor <paulpastor@me.com>
2025-12-30 10:44:19 -0500

    added function calling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.call_function import available_functions
 from prompts import system_prompt
 
 load_dotenv()
@@ -22,10 +23,13 @@ args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
+
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=types.GenerateContentConfig(
+        system_instruction=system_prompt, tools=[available_functions]
+    ),
 )
 
 if response.usage_metadata:
@@ -33,6 +37,11 @@ if response.usage_metadata:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+
+    if response.function_calls is not None:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
 else:
     raise RuntimeError("No usage metadata found in the response.")
